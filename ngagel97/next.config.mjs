724560a792879f03561e3ff768519e43e74f4d2d
jou170 <130060544+jou170@users.noreply.github.com>
2024-11-30 21:38:03 +0700

    ubah next config

--- ngagel97/next.config.mjs
+++ ngagel97/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET, // Mengekspos ke klien
+  },
+};
 
 export default nextConfig;
