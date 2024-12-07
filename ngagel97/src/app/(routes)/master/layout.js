40d09ae2dab2606fe0c33731a5757366d8424b41
jou170 <130060544+jou170@users.noreply.github.com>
2024-12-07 15:00:59 +0700

    navbar master baru

--- ngagel97/src/app/(routes)/master/layout.js
+++ ngagel97/src/app/(routes)/master/layout.js
@@ -1,14 +1,36 @@
-import MasterNavbar from "./components/MasterNavbar";
+"use client";
 
-export default function MasterLayout({
-  children, // will be a page or nested layout
-}) {
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import MasterAppBar from "./components/MasterAppBar";
+import MasterDrawer from "./components/MasterDrawer";
+
+const drawerWidth = 240;
+
+export default function MasterLayout({ children }) {
   return (
-    <section>
-      {/* Include shared UI here e.g. a header or sidebar */}
-      <MasterNavbar />
-
-      {children}
-    </section>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <MasterAppBar/>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <MasterDrawer/>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
   );
 }
